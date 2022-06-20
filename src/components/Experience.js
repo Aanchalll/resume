82340fd5ac6ef4f874fb15448f1639e2e56fe914
biz-Aanchal <98saanchal@gmail.com>
2022-06-20 12:49:53 +0530

    initial

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,18 +1,100 @@
-import { Box ,Typography,Divider} from "@mui/material";
+import React from "react";
+import {
+  Box,
+  Typography,
+  Divider,
+  ListItem,
+  List,
+  ListItemAvatar,
+  Avatar,
+  ListItemText,
+} from "@mui/material";
 
 export default function Experience() {
-  return <Box id="experience" class="heading-primary card-padding">
-  <Divider />
+  return (<>  <Divider />
+    <Box id="experience" class="heading-primary card-padding">
+      <ListItem alignItems="flex-start" className="heading-primary-sub-left">
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/reactsign.png" />
+        </ListItemAvatar>
+        <ListItemText primary="Brunch this weekend?" />
+      </ListItem>
+     
+        <ListItem alignItems="flex-start" className="heading-primary-sub-left">
+          <ListItemAvatar>
+            <Avatar alt="Remy Sharp" src="/reactsign.png" />
+          </ListItemAvatar>
+          <ListItemText
+            primary="Brunch this weekend?"
+            secondary={
+              <React.Fragment>
+                <Typography
+                  sx={{ display: "inline" }}
+                  component="span"
+                  variant="body2"
+                  color="text.primary"
+                >
+                  Ali Connors
+                </Typography>
+                {
+                  " — In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available."
+                }
+              </React.Fragment>
+            }
+          />
+        </ListItem>
+        <ListItem alignItems="flex-start" className="heading-primary-sub-left">
+          <ListItemAvatar>
+            <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+          </ListItemAvatar>
+          <ListItemText
+            primary="Summer BBQ"
+            secondary={
+              <React.Fragment>
+                <Typography
+                  sx={{ display: "inline" }}
+                  component="span"
+                  variant="body2"
+                  color="text.primary"
+                >
+                  to Scott, Alex, Jennifer
+                </Typography>
+                " — In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available."
 
-  <Typography variant='h6' className="heading-primary-sub" >In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</Typography>
+              </React.Fragment>
+            }
+          />
+        </ListItem>
+        <ListItem alignItems="flex-start" className="heading-primary-sub-left">
+          <ListItemAvatar>
+            <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+          </ListItemAvatar>
+          <ListItemText
+            primary="Oui Oui"
+            secondary={
+              <React.Fragment>
+                <Typography
+                  sx={{ display: "inline" }}
+                  component="span"
+                  variant="body2"
+                  color="text.primary"
+                >
+                  Sandra Adams
+                </Typography>
+                " — In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available."
+
+              </React.Fragment>
+            }
+          />
+        </ListItem>
+    </Box></>
+  );
+}
+ {/* <Typography variant='h6' className="heading-primary-sub" ></Typography>
   <Typography variant='h6' className="heading-primary-sub" >Aanchal Sahu</Typography>
   <Typography variant='h6' className="heading-primary-sub-left">Aanchal Sahu</Typography>
   <Typography variant='h6' className="heading-primary-sub-left">Aanchal Sahu</Typography>
   <Typography variant='h6' className="heading-primary-sub">Aanchal Sahu</Typography>
   <Typography variant='h6' className="heading-primary-sub">Aanchal Sahu</Typography>
   <Typography variant='h6' className="heading-primary-sub-left">Aanchal Sahu</Typography>
-  <Typography variant='h6' className="heading-primary-sub-left">Aanchal Sahu</Typography>
-  <Divider />
-
-  </Box>;
-}
+  <Typography variant='h6' className="heading-primary-sub-left">Aanchal Sahu</Typography> */}
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,14 +1,60 @@
-import { Box ,Typography,Divider} from "@mui/material";
+import React from "react";
+import {
+  Box,
+  Typography,
+  Divider,
+  ListItem,
+  List,
+  ListItemAvatar,
+  Avatar,
+  ListItemText,
+} from "@mui/material";
 
 export default function Skills() {
-  return <Box id="skills" class="heading-primary card-padding">
-  <Typography variant='h6' className="heading-primary-sub" >Aanchal Sahu</Typography>
-  <Typography variant='h6' className="heading-primary-sub" >Aanchal Sahu</Typography>
-  <Typography variant='h6' className="heading-primary-sub-left">Aanchal Sahu</Typography>
-  <Typography variant='h6' className="heading-primary-sub-left">Aanchal Sahu</Typography>
-  <Typography variant='h6' className="heading-primary-sub">Aanchal Sahu</Typography>
-  <Typography variant='h6' className="heading-primary-sub">Aanchal Sahu</Typography>
-  <Typography variant='h6' className="heading-primary-sub-left">Aanchal Sahu</Typography>
-  <Typography variant='h6' className="heading-primary-sub-left">Aanchal Sahu</Typography>
-  </Box>;
+  return (
+    <Box id="skills" class="heading-primary card-padding">
+      <ListItem alignItems="center" className="heading-primary-sub">
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/reactsign.png" />
+        </ListItemAvatar>
+        <ListItemText primary="Brunch this weekend?" />
+      </ListItem>
+      <ListItem alignItems="center" className="heading-primary-sub">
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/nodejs_logo.png" />
+        </ListItemAvatar>
+        <ListItemText primary="Brunch this weekend?" />
+      </ListItem><ListItem alignItems="center" className="heading-primary-sub">
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/java.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="Brunch this weekend?" />
+      </ListItem><ListItem alignItems="center" className="heading-primary-sub">
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/mysql_PNG35.png" />
+        </ListItemAvatar>
+        <ListItemText primary="Brunch this weekend?" />
+      </ListItem><ListItem alignItems="center" className="heading-primary-sub">
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/sql.png" />
+        </ListItemAvatar>
+        <ListItemText primary="Brunch this weekend?" />
+      </ListItem><ListItem alignItems="center" className="heading-primary-sub">
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/html-icon-png-13.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="Brunch this weekend?" />
+      </ListItem><ListItem alignItems="center" className="heading-primary-sub">
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/css.png" />
+        </ListItemAvatar>
+        <ListItemText primary="Brunch this weekend?" />
+      </ListItem><ListItem alignItems="center" className="heading-primary-sub">
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/JS.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="Brunch this weekend?" />
+      </ListItem>
+    </Box>
+  );
 }
